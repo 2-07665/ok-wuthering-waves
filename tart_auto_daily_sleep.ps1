cb6384842fcfb88e322e5a7787e13a2f6e2c00f4
2-07665 <941275963@qq.com>
2025-11-30 00:03:09 -0500

    initial commit

---
+++ tart_auto_daily_sleep.ps1
@@ -0,0 +1,10 @@
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path; Set-Location $root
+$p = Start-Process "$root/.venv/Scripts/python.exe" "auto_daily.py" -WorkingDirectory $root -PassThru
+Wait-Process -Id $p.Id -Timeout 1200
+if (Get-Process -Id $p.Id -ErrorAction SilentlyContinue) {
+    Stop-Process -Id $p.Id -Force
+    shutdown /s /t 0
+}
+else {
+    rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+}
